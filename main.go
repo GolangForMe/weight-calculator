90f3c4f70babb89504ce37b43d93b5d9eb31293f
Valery Semenenko <8580397+gearmobile@users.noreply.github.com>
2026-09-09 21:39:49 +0300

    add input

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ import (
 )
 
 func main() {
-	weight := 70
-	height := 1.7
+	var (
+		weight int
+		height float64
+	)
+
+	fmt.Print("Введите свой вес в килограммах (180): ")
+	fmt.Scan(&weight)
+
+	fmt.Print("Введите свой рост в метрах (1.7): ")
+	fmt.Scan(&height)
+
 	imt := calc.Calc(weight, height)
 
 	fmt.Printf("Индекс веса равен %f\n", imt)
