3b82eaf4ae5e964afa7c16ad4fbf9f5d4485f7a9
Valery Semenenko <vsemenenko@sigma-it.ru>
2026-09-10 16:25:30 +0300

    feat: add functions

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 	"github.com/gearmobile/calculator/check"
 )
 
-func main() {
+func getUserInput() (int, float64) {
 	var (
 		weight int     = 80
 		height float64 = 1.7
 	)
-
-	fmt.Println("___ Калькулятор индекса массы тела ___")
 
 	fmt.Print("Введите свой вес в килограммах (80): ")
 	fmt.Scan(&weight)
@@ -21,8 +19,17 @@ func main() {
 	fmt.Print("Введите свой рост в сантиметрах (170): ")
 	fmt.Scan(&height)
 
-	imt := calc.Calc(weight, height)
+	return weight, height
+}
 
+func printImt(imt float64) {
 	fmt.Printf("Индекс веса равен %0.2f\n", imt)
 	fmt.Println(check.Check(imt))
+}
+
+func main() {
+	fmt.Println("___ Калькулятор индекса массы тела ___")
+	weight, height := getUserInput()
+	imt := calc.Calc(weight, height)
+	printImt(imt)
 }
